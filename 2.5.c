63b402620ede69e89f7203ef2de227f392d4d1f6
FatinAniZahidi <haziqahfatinani@gmail.com>
2021-05-02 15:11:08 +0800

    commit for 3.5

---
+++ 2.5.c
@@ -0,0 +1,83 @@
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <signal.h>
+
+int main(){
+	
+	void sigint_handler(int sig);
+	int i, flag = 0;
+	int pipefds[2];
+	int num;
+	int buffer;
+
+	if(signal(SIGINT,sigint_handler) == SIG_ERR){
+		perror("signal");
+		exit(1);
+	}
+    	
+	if(pipe(pipefds) == -1){
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
+
+	pid_t pid = fork();
+
+	if(pid == 0){
+	printf("Enter a number: \n");
+	scanf("%d", &num);
+	close(pipefds[0]);
+	write(pipefds[1],&num, sizeof(num));
+	}
+	
+	else if(pid > 0){
+	wait(NULL);
+	close(pipefds[1]);
+	read(pipefds[0], &buffer, sizeof(buffer));
+	
+
+	for(i = 2; i <= buffer / 2; i++){
+	
+	if(buffer % i == 0){
+		flag = 1;
+		break;
+		}
+	}
+
+	if (buffer == 1){
+		printf("1 is a prime number.\n", buffer);
+	}
+	else{
+		if(flag == 0)
+			printf("%d is a prime number.\n", buffer);
+		else
+			printf("%d is not a prime number.\n", buffer);
+		}
+	}
+
+		return EXIT_SUCCESS;
+	}
+
+void sigint_handler(int sig){
+	printf("Please Try Again.\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
